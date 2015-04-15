d1bdcfe5cbea07da683ba55138b6a5fc7ae5713a
Derek Bowen <debowen@deloitte.com>
2015-04-14 23:19:02 -0600

    Added encoding parameter to initializer.

--- SwiftCSV/CSV.swift
+++ SwiftCSV/CSV.swift
@@ -14,8 +14,8 @@ public class CSV {
     public var columns = Dictionary<String, [String]>()
     var delimiter = NSCharacterSet(charactersInString: ",")
     
-    public init?(contentsOfURL url: NSURL, delimiter: NSCharacterSet, error: NSErrorPointer) {
-        let csvString = String(contentsOfURL: url, encoding: NSUTF8StringEncoding, error: error);
+    public init?(contentsOfURL url: NSURL, delimiter: NSCharacterSet, encoding: UInt, error: NSErrorPointer) {
+        let csvString = String(contentsOfURL: url, encoding: encoding, error: error);
         if let csvStringToParse = csvString {
             self.delimiter = delimiter
             
@@ -29,9 +29,9 @@ public class CSV {
         }
     }
     
-    public convenience init?(contentsOfURL url: NSURL, error: NSErrorPointer) {
+    public convenience init?(contentsOfURL url: NSURL, encoding: UInt = NSUTF8StringEncoding, error: NSErrorPointer) {
         let comma = NSCharacterSet(charactersInString: ",")
-        self.init(contentsOfURL: url, delimiter: comma, error: error)
+        self.init(contentsOfURL: url, delimiter: comma, encoding: encoding, error: error)
     }
     
     func parseHeaders(fromLines lines: [String]) -> [String] {
