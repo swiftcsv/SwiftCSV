cabc553083571ac396a73d90437c1bf8f0951760
Will Richardson <william.hamish@gmail.com>
2016-04-11 22:19:42 +1200

    Split into different files

--- SwiftCSV/CSV.swift
+++ SwiftCSV/CSV.swift
@@ -12,13 +12,13 @@ public class CSV {
     static private let comma: Character = ","
     
     public var header: [String]!
-    private var _rows: [[String: String]]? = nil
-    private var _columns: [String: [String]]? = nil
+    var _rows: [[String: String]]? = nil
+    var _columns: [String: [String]]? = nil
     
-    private var text: String
-    private var delimiter: Character
+    var text: String
+    var delimiter: Character
     
-    private let loadColumns: Bool
+    let loadColumns: Bool
     
     public init(string: String, delimiter: Character = comma, loadColumns: Bool = true) {
         self.text = string
@@ -42,116 +42,5 @@ public class CSV {
     
     public func dataUsingEncoding(encoding: NSStringEncoding) -> NSData? {
         return description.dataUsingEncoding(encoding)
-    }
-}
-
-extension CSV {
-    public var rows: [[String: String]] {
-        if _rows == nil {
-            parse()
-        }
-        return _rows!
-    }
-    public var columns: [String: [String]] {
-        if !loadColumns {
-            return [:]
-        } else if _columns == nil {
-            parse()
-        }
-        return _columns!
-    }
-    
-    public func enumerateAsDict(block: [String: String] -> ()) {
-        var first = true
-        let enumeratedHeader = header.enumerate()
-        
-        text.enumerateLines { line, _ in
-            if !first {
-                let fields = self.parseLine(line)
-                var dict = [String: String]()
-                for (index, head) in enumeratedHeader {
-                    dict[head] = index < fields.count ? fields[index] : ""
-                }
-                block(dict)
-            } else {
-                first = false
-            }
-        }
-    }
-    public func enumerateAsArray(block: [String] -> ()) {
-        var first = true
-        text.enumerateLines { line, _ in
-            if !first {
-                block(self.parseLine(line))
-            } else {
-                first = false
-            }
-        }
-    }
-    
-    private func parse() {
-        var rows = [[String: String]]()
-        var columns = [String: [String]]()
-
-        if loadColumns {
-            for head in header {
-                columns[head] = []
-            }
-        }
-        
-        enumerateAsDict { fields in
-            if self.loadColumns {
-                for (key, value) in fields {
-                    columns[key]?.append(value)
-                }
-            }
-            rows.append(fields)
-        }
-        _rows = rows
-        _columns = columns
-    }
-    
-    private func parseLine(line: String) -> [String] {
-        let escape: Character = "\\"
-        let quote: Character = "\""
-        
-        var fields = [String]()
-        
-        var inQuotes = false
-        var currentIndex = line.startIndex
-        
-        var field = [Character]()
-        
-        while currentIndex < line.endIndex {
-            let char = line[currentIndex]
-            if !inQuotes && char == self.delimiter {
-                fields.append(String(field))
-                field = [Character]()
-            } else {
-                if char == quote {
-                    inQuotes = !inQuotes
-                } else if char == escape {
-                    currentIndex = currentIndex.successor()
-                    field.append(line[currentIndex])
-                } else {
-                    field.append(char)
-                }
-            }
-            currentIndex = currentIndex.successor()
-        }
-        fields.append(String(field))
-        
-        return fields
-    }
-}
-
-extension CSV: CustomStringConvertible {
-    public var description: String {
-        let head = header.joinWithSeparator(",") + "\n"
-        
-        let cont = rows.map { row in
-            header.map { row[$0]! }.joinWithSeparator(",")
-        }.joinWithSeparator("\n")
-        return head + cont
     }
 }
---
+++ SwiftCSV/Parser.swift
@@ -0,0 +1,109 @@
+//
+//  Parser.swift
+//  SwiftCSV
+//
+//  Created by Will Richardson on 11/04/16.
+//  Copyright © 2016 JavaNut13. All rights reserved.
+//
+
+import Foundation
+
+extension CSV {
+    public var rows: [[String: String]] {
+        if _rows == nil {
+            parse()
+        }
+        return _rows!
+    }
+    public var columns: [String: [String]] {
+        if !loadColumns {
+            return [:]
+        } else if _columns == nil {
+            parse()
+        }
+        return _columns!
+    }
+    
+    public func enumerateAsDict(block: [String: String] -> ()) {
+        var first = true
+        let enumeratedHeader = header.enumerate()
+        
+        text.enumerateLines { line, _ in
+            if !first {
+                let fields = self.parseLine(line)
+                var dict = [String: String]()
+                for (index, head) in enumeratedHeader {
+                    dict[head] = index < fields.count ? fields[index] : ""
+                }
+                block(dict)
+            } else {
+                first = false
+            }
+        }
+    }
+    public func enumerateAsArray(block: [String] -> ()) {
+        var first = true
+        text.enumerateLines { line, _ in
+            if !first {
+                block(self.parseLine(line))
+            } else {
+                first = false
+            }
+        }
+    }
+    
+    private func parse() {
+        var rows = [[String: String]]()
+        var columns = [String: [String]]()
+        
+        if loadColumns {
+            for head in header {
+                columns[head] = []
+            }
+        }
+        
+        enumerateAsDict { fields in
+            if self.loadColumns {
+                for (key, value) in fields {
+                    columns[key]?.append(value)
+                }
+            }
+            rows.append(fields)
+        }
+        _rows = rows
+        _columns = columns
+    }
+    
+    func parseLine(line: String) -> [String] {
+        let escape: Character = "\\"
+        let quote: Character = "\""
+        
+        var fields = [String]()
+        
+        var inQuotes = false
+        var currentIndex = line.startIndex
+        
+        var field = [Character]()
+        
+        while currentIndex < line.endIndex {
+            let char = line[currentIndex]
+            if !inQuotes && char == self.delimiter {
+                fields.append(String(field))
+                field = [Character]()
+            } else {
+                if char == quote {
+                    inQuotes = !inQuotes
+                } else if char == escape {
+                    currentIndex = currentIndex.successor()
+                    field.append(line[currentIndex])
+                } else {
+                    field.append(char)
+                }
+            }
+            currentIndex = currentIndex.successor()
+        }
+        fields.append(String(field))
+        
+        return fields
+    }
+}
