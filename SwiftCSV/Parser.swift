777474f8cf1f9558ece76b544779baf14e8e0e5c
Christian Tietze <christian.tietze@gmail.com>
2016-10-25 08:37:18 +0200

    extract parsing state machine

--- SwiftCSV/Parser.swift
+++ SwiftCSV/Parser.swift
@@ -6,12 +6,6 @@
 //  Copyright © 2016 Naoto Kaneko. All rights reserved.
 //
 
-fileprivate extension Character {
-    var isNewline: Bool {
-        return self == "\n" || self == "\r\n"
-    }
-}
-
 extension CSV {
     /// Parse the file and call a block on each row, passing it in as a list of fields
     /// limitTo will limit the result to a certain number of lines
@@ -43,11 +37,6 @@ extension CSV {
         var currentIndex = text.startIndex
         let endIndex = text.endIndex
 
-        var atStart = true
-        var parsingField = false
-        var parsingQuotes = false
-        var innerQuotes = false
-
         var fields = [String]()
         var field = ""
 
@@ -63,77 +52,14 @@ extension CSV {
             field = ""
         }
 
-        func changeState(_ char: Character) {
-            if atStart {
-                if char == "\"" {
-                    atStart = false
-                    parsingQuotes = true
-                } else if char == delimiter {
-                    fields.append(field)
-                    field = ""
-                } else if char.isNewline {
-                    finishRow()
-                } else {
-                    parsingField = true
-                    atStart = false
-                    field.append(char)
-                }
-            } else if parsingField {
-                if innerQuotes {
-                    if char == "\"" {
-                        field.append(char)
-                        innerQuotes = false
-                    } else {
-                        fatalError("Can't have non-quote here: \(char)")
-                    }
-                } else {
-                    if char == "\"" {
-                        innerQuotes = true
-                    } else if char == delimiter {
-                        atStart = true
-                        parsingField = false
-                        innerQuotes = false
-                        fields.append(field)
-                        field = ""
-                    } else if char.isNewline {
-                        atStart = true
-                        parsingField = false
-                        innerQuotes = false
-                        finishRow()
-                    } else {
-                        field.append(char)
-                    }
-                }
-            } else if parsingQuotes {
-                if innerQuotes {
-                    if char == "\"" {
-                        field.append(char)
-                        innerQuotes = false
-                    } else if char == delimiter {
-                        atStart = true
-                        parsingField = false
-                        innerQuotes = false
-                        fields.append(field)
-                        field = ""
-                    } else if char.isNewline {
-                        atStart = true
-                        parsingQuotes = false
-                        innerQuotes = false
-                        finishRow()
-                    } else {
-                        fatalError("Can't have non-quote here: \(char)")
-                    }
-                } else {
-                    if char == "\"" {
-                        innerQuotes = true
-                    } else {
-                        field.append(char)
-                    }
-                }
-            } else {
-                fatalError("me_irl")
-            }
-        }
+        var state: ParsingState = ParsingState(
+            delimiter: delimiter,
+            finishRow: finishRow,
+            appendChar: { field.append($0) },
+            finishField: {
+                fields.append(field)
+                field = ""
+        })
 
         func limitReached(_ count: Int) -> Bool {
 
@@ -147,7 +73,7 @@ extension CSV {
         while currentIndex < endIndex {
             let char = text[currentIndex]
 
-            changeState(char)
+            state.change(char)
 
             if limitReached(count) {
                 break
