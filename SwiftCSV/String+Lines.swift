d43d40a76b3364a3bdcc31fcf9c24db272f1fd02
Christian Tietze <christian.tietze@gmail.com>
2019-04-25 19:39:48 +0200

    update newline detection for macOS

--- SwiftCSV/String+Lines.swift
+++ SwiftCSV/String+Lines.swift
@@ -7,11 +7,17 @@
 //
 
 extension String {
-    var firstLine: String {
+    internal var firstLine: String {
         var current = startIndex
-        while current < endIndex && self[current] != "\r\n" && self[current] != "\n" && self[current] != "\r" {
+        while current < endIndex && self[current].isNewline == false {
             current = self.index(after: current)
         }
         return String(self[..<current])
+    }
+}
+
+extension Character {
+    internal var isNewline: Bool {
+        return self == "\n" || self == "\r\n" || self == "\r"
     }
 }
