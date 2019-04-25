9300b4baa9b092d6503200cc7bf6395f54c0e9d7
Christian Tietze <christian.tietze@gmail.com>
2019-04-25 18:34:04 +0200

    Swift 3 enumeration improvements (#69)
    * rename columns to namedColumns
    
    * move public interface properties into CSV.swift
    
    * move "named" field access to NamedView
    
    The parse() helper method masked the fact that rows and columns
    used to be lazily initialized; with NamedView as the base for both
    computed properties, a lazy initializer works just as well.
    
    * refactor enumerateAsArray into a static function
    
    * change CSV.header to be computable in the initializer
    
    The new static enumeration function helps make it possible to not
    keep `header` as a mutable force-unwrapped optional.
    
    * fix Swift documentation
    
    * rename parsing elements
    
    * refactor parse helpers
    
    * extract parsing state machine
    
    * rename CSV.rows to .namedRows
    
    * add EnumeratedView as a complementary to hash-based storage

--- SwiftCSV/ParserHelpers.swift
+++ SwiftCSV/ParserHelpers.swift
@@ -7,29 +7,11 @@
 //
 
 extension CSV {
-    /// List of dictionaries that contains the CSV data
-    public var rows: [[String: String]] {
-        if _rows == nil {
-            parse()
-        }
-        return _rows!
-    }
-    
-    /// Dictionary of header name to list of values in that column
-    /// Will not be loaded if loadColumns in init is false
-    public var columns: [String: [String]] {
-        if !loadColumns {
-            return [:]
-        } else if _columns == nil {
-            parse()
-        }
-        return _columns!
-    }
-    
+
     /// Parse the file and call a block for each row, passing it as a dictionary
-    public func enumerateAsDict(_ block: @escaping ([String: String]) -> ()) {
+    public func enumerateAsDict(_ block: @escaping ([String : String]) -> ()) {
         let enumeratedHeader = header.enumerated()
-        
+
         enumerateAsArray { fields in
             var dict = [String: String]()
             for (index, head) in enumeratedHeader {
@@ -44,21 +26,4 @@ extension CSV {
         self.enumerateAsArray(block, limitTo: nil, startAt: 1)
     }
     
-    fileprivate func parse() {
-        var rows = [[String: String]]()
-        var columns = [String: [String]]()
-        
-        enumerateAsDict { dict in
-            rows.append(dict)
-        }
-
-        if loadColumns {
-            for field in header {
-                columns[field] = rows.map { $0[field] ?? "" }
-            }
-        }
-        
-        _columns = columns
-        _rows = rows
-    }
 }
---
+++ SwiftCSVTests/EnumeratedViewTests.swift
@@ -0,0 +1,59 @@
+//
+//  EnumeratedViewTests.swift
+//  SwiftCSV
+//
+//  Created by Christian Tietze on 2016-10-25.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftCSV
+
+class EnumeratedViewTests: XCTestCase {
+
+    var csv: CSV!
+
+    override func setUp() {
+        super.setUp()
+
+        csv = CSV(string: "id,name,age\n1,Alice,18\n2,Bob,19\n3,Charlie,20", delimiter: ",", loadColumns: true)
+    }
+
+    func testExposesRows() {
+        let expected: [[String]] = [
+            ["1", "Alice", "18"],
+            ["2", "Bob", "19"],
+            ["3", "Charlie", "20"]
+        ]
+        let actual = csv.enumeratedRows
+
+        // Abort if counts don't match to not raise index-out-of-bounds exception
+        guard actual.count == expected.count else {
+            XCTFail("expected actual.count to equal expected.count")
+            return
+        }
+
+        for i in actual.indices {
+            XCTAssertEqual(actual[i], expected[i])
+        }
+    }
+
+    func testExposesColumns() {
+        let actual = csv.enumeratedColumns
+
+        // Abort if counts don't match to not raise index-out-of-bounds exception
+        guard actual.count == 3 else {
+            XCTFail("expected actual.count to equal 3")
+            return
+        }
+
+        XCTAssertEqual(actual[0].header, "id")
+        XCTAssertEqual(actual[0].rows, ["1", "2", "3"])
+
+        XCTAssertEqual(actual[1].header, "name")
+        XCTAssertEqual(actual[1].rows, ["Alice", "Bob", "Charlie"])
+
+        XCTAssertEqual(actual[2].header, "age")
+        XCTAssertEqual(actual[2].rows, ["18", "19", "20"])
+    }
+}
