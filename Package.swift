5e5f6c28cc9a791c0c0fd821e555549d7fae9161
Christian Tietze <christian.tietze@gmail.com>
2019-06-25 18:27:54 +0200

    specify Swift v5 for SwiftPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -11,10 +10,7 @@ let package = Package(
             name: "SwiftCSV",
             targets: ["SwiftCSV"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    swiftLanguageVersions: [.v5],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
