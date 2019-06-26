35f91d6f3068f82adf74b57677e5c439e9a2d30b
Christian Tietze <christian.tietze@gmail.com>
2019-06-26 17:11:58 +0200

    add platform versions to Package manifest

--- Package.swift
+++ Package.swift
@@ -4,12 +4,16 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftCSV",
+    platforms: [
+        .macOS("10.10"), .iOS("8.0"), .tvOS("9.2"), .watchOS("2.2")
+    ],
+    swiftLanguageVersions: [.v5, .v4_2],
+    dependencies: [], // No dependencies
     products: [
         .library(
             name: "SwiftCSV",
             targets: ["SwiftCSV"]),
     ],
-    dependencies: [], // No dependencies
     targets: [
         .target(
             name: "SwiftCSV",
