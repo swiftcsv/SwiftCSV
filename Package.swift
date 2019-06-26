d56b3b982a2d7942f8e9a5a84f9fa65853797d86
Christian Tietze <christian.tietze@gmail.com>
2019-06-26 07:56:16 +0200

    add empty dependencies to Package.swift

--- Package.swift
+++ Package.swift
@@ -5,17 +5,16 @@ import PackageDescription
 let package = Package(
     name: "SwiftCSV",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftCSV",
             targets: ["SwiftCSV"]),
     ],
-    swiftLanguageVersions: [.v5],
+    dependencies: [], // No dependencies
+    swiftLanguageVersions: [.v4, .v5],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftCSV",
+            dependencies: [],
             path: "SwiftCSV"),
         .testTarget(
             name: "SwiftCSVTests",
