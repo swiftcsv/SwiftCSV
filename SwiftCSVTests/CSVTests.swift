22dc4dd1272e990da64ea87a8bc84bb606eb177e
Christian Tietze <christian.tietze@gmail.com>
2019-08-21 15:51:01 +0200

    add test for throw when parsing HTML
    See issue #80

--- SwiftCSVTests/CSVTests.swift
+++ SwiftCSVTests/CSVTests.swift
@@ -108,4 +108,13 @@ class CSVTests: XCTestCase {
             XCTAssertEqual(expected[index], row)
         }
     }
+
+    func testThrowsOnInvalidData() {
+        do {
+            _ = try CSV(string: "\n\n<html lang=\"en\">\n \n <meta charset=\"utf-8\"></html>", delimiter: ",", loadColumns: false)
+            XCTFail("Expected to throw")
+        } catch {
+            XCTAssert(error is CSVParseError)
+        }
+    }
 }
